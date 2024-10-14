cd834249b1c3dd888b522773f333d146124488f1
WisdomerLee <50577439+WisdomerLee@users.noreply.github.com>
2024-10-14 09:54:02 +0900

    Neural Network에서 사용할 함수 설정

--- Neural Network/Project/nn_scratch_start.py
+++ Neural Network/Project/nn_scratch_start.py
@@ -25,3 +25,37 @@ scaler = StandardScaler()
 X_train_scale = scaler.fit_transform(X_train) 
 X_test_scale = scaler.transform(X_test) 
 
+class NeuralNetworkFromScratch:
+  def __init__(self, LR, X_train, y_train, X_test, y_test):
+    self.w = np.random.randn(X_train_scale.shape[1])
+    self.b = np.random.randn()
+    self.LR = LR
+    self.X_train = X_train
+    self.y_train = y_train
+    self.X_test = X_test
+    self.y_test = y_test
+    self.L_train = []
+    self.L_test = []
+
+  # Helper function
+  def activation(self, x):
+    #sigmoid
+    return 1/ (1+np.exp(-x))
+  
+  def dactivation(self, x):
+    # derivative of sigmoid
+    return self.activation(x) * (1-self.activation(x))
+
+  # forward function
+  def forward(self, X):
+    hidden_1 = np.dot(X, self.w) + self.b
+    activate_1 = self.activation(hidden_1)
+    return activate_1
+  # backward function
+  def backward(self, X, y_true):
+    # gradient 계산
+    hidden_1 = np.dot(X, self.w) + self.b
+    y_pred = self.forward(X)
+    dL_dpred 
+    
+    
