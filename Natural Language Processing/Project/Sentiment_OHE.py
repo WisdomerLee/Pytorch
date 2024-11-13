7a6a581dc800a10d5a5e7316b05fc601154fb335
WisdomerLee <50577439+WisdomerLee@users.noreply.github.com>
2024-11-14 08:35:20 +0900

    문자, 글자에 숨어있는 감정 분류하기!
    감정 분류하기
    one-hot encoding으로 분석 시도

--- Natural Language Processing/Project/Sentiment_OHE.py
+++ Natural Language Processing/Project/Sentiment_OHE.py
@@ -57,3 +57,58 @@ test_ds = SentimentData(X=X_test_onehot, y=y_test)
 
 train_loader = DataLoader(dataset=train_ds, batch_size=BATCH_SIZE, shuffle=True)
 test_loader = DataLoader(dataset=test_ds, batch_size=15000)
+
+class SentimentModel(nn.Module):
+         def __init__(self, NUM_FEATURES, NUM_CLASSES, HIDDEN=10):
+                  super().__init__()
+                  self.linear = nn.Linear(NUM_FEATURES, HIDDEN)
+                  self.linear2 = nn.Linear(HIDDEN, NUM_CLASSES)
+                  self.relu = nn.ReLU()
+                  self.log_softmax = nn.LogSoftmax(dim=1)
+
+         def forward(self, x):
+                  x = self.linear(x)
+                  x = self.relu(x)
+                  x = self.linear2(x)
+                  x = self.log_softmax(x)
+                  return x
+
+
+model = SentimentModel(NUM_FEATURES=X_train_onehot.shape[1], NUM_CLASSES=3)
+
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters())
+
+train_losses = []
+for e in range(NUM_EPOCHS):
+         curr_loss = 0
+         for X_batch, y_batch in train_loader:
+                  optimizer.zero_grad()
+                  y_pred_log = model(X_batch)
+                  loss = criterion(y_pred_log, y_batch.long())
+
+                  curr_loss += loss.item()
+                  loss.backward()
+                  optimizer.step()
+
+         train_losses.append(curr_loss)
+         print(f"Epoch {e}, Loss: {curr_loss}")
+
+
+sns.lineplot(x=list(range(len(train_losses))), y=train_losses)
+plt.show()
+
+
+with torch.no_grad():
+         for X_batch, y_batch in test_loader:
+                  y_test_pred_log = model(X_batch)
+                  y_test_pred = torch.argmax(y_test_pred_log, dim=1)
+
+y_test_pred_np = y_test_pred.squeeze().cpu().numpy()
+
+acc = accuracy_score(y_pred=y_test_pred_np, y_true=y_test)
+
+most_common_cnt = Counter(y_test).most_common()[0][1]
+print(f"Naive Classifier: {np.round(most_common_cnt / len(y_test) * 100, 1)} %")
+
+sns.heatmap(confusion_matrix(y_test_pred_np, y_test), annot=True, fmt=".0f")
