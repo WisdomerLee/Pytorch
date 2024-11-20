49de8199f29f8cf569451acb5b66c3a3317ab086
WisdomerLee <50577439+WisdomerLee@users.noreply.github.com>
2024-11-21 08:04:46 +0900

    Extreme Learning Machine
    데이터가 자주 바뀌어서 훈련을 자주해야 한다면 한 번 고려해볼만 한 것
    정확성이 아주아주 높진 않지만, 훈련 시간이 짧은 것에 비해 정확성이 높은 편에 속함

--- Miscellanious Topics/Project/elm.py
+++ Miscellanious Topics/Project/elm.py
@@ -19,3 +19,39 @@ HIDDEN_SIZE = 100 # 숨은 layer에 쓰이는 neuron의 갯수!
 
 input_weights = np.random.normal(size=[INPUT_SIZE, HIDDEN_SIZE])
 bias = np.random.normal(size=[HIDDEN_SIZE])
+
+def relu(x):
+  return np.maximum(x, 0, x) # relu의 정의를 그대로 이용한 함수 > 음수 부분은 0, 양수는 그대로 출력하는 relu함수를 python으로 정의한 것 > 원래는 torch의 내부 함수로 있으나 지금은 torch를 불러오지 않은 상태이므로 relu를 정의하여 쓰는 것
+
+def hidden_nodes(X):
+  G = np.dot(X, input_weights) # Linear Algebra의 방식대로 계산하도록 설정 - matrix 곱으로 진행하는 Linear Regression의 형태 그대로
+  G = G + biases # bias 보정을 더한 것 > ax+b의 선형 관계를 적용하고 > nn.Linear()와 같은 역할
+  H = relu(G) # relu함수 적용 > activation 함수 적용
+  return H
+
+# 매우매우 간단한 형태의 관계이기 때문에 연산이 빠름
+
+beta = np.dot(linalg.pinv(hidden_nodes(X_train)), y_train) # output에 적용할 weight 계산 > 이것이 실질적인 모델의 파라미터 역할을 수행
+# numpy의 linalg는 선형대수의 함수를 포함하고 있고, pinv함수는 pseudo-inverse matrix를 계산함 > 역행렬과 같은 형태를 하는 함수를 계산해냄 > 해당 것도 역시 torch의 모듈을 이용하면 loss의 backward()라는 함수 내에 포함되어 있는 과정
+
+def predict(X):
+  out = hidden_nodes(X)
+  out = np.dot(out, beta)
+  return out
+
+# 평가!
+y_test_pred = predict(X_test)
+correct = 0
+total = X_test.shape[0]
+
+for i in range(total):
+  predicted = np.round(y_test_pred[i], 0)
+  y_test_true = y_test[i]
+  correct += 1 if predicted == y_test_true else 0
+
+accuracy = correct/total
+print(f"Accuracy for {HIDDEN_SIZE} hidden nodes: {accuracy}")
+
+cnt = Counter(y_test)
+
+np.max(list(cnt.values())) / total
